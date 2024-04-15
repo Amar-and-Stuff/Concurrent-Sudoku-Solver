4864f1d83d9579b190e466384e3bc0f2c7f705a6
Mounish <mounishmaram2002@gmail.com>
2024-04-15 15:39:01 +0530

    created validation methods

--- SolverThread.java
+++ SolverThread.java
@@ -1,13 +1,34 @@
+import java.util.HashSet;
+
 public class SolverThread extends Thread{
     private int xpos;
     private int ypos;
-    private int sudoku[][]=new int[9][9];
-    SolverThread(int sudoku,int xpos,int ypos) {
+    private int sudoku[][];
+    private HashSet<Integer> hs;
+    SolverThread(int[][] sudoku,int xpos,int ypos) {
         this.xpos=xpos;
         this.ypos=ypos;
         this.sudoku=sudoku;
+        hs=new HashSet<>();
+        for(int i=1;i<=9;i++) {
+            hs.add(i);
+        }
     }
     public void run() {
+        
+    }
+    public void check() {
+        validateRow();
+        validateColumn();
+        validateMatrix();
+    }
+    public void validateRow() {
+        
+    }
+    public void validateColumn() {
+        
+    }
+    public void validateMatrix() {
         
     }
 
