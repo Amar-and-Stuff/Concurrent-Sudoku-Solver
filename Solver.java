3cb3d15147e74832823a66af59d4a45208afe850
amar-and-stuff <superbumper000@gmail.com>
2024-04-15 17:59:19 +0530

    finalized the errors

--- Solver.java
+++ Solver.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.StringJoiner;
 
 public class Solver {
     private int[][] sudoku;
@@ -14,30 +14,32 @@ public class Solver {
     }
 
     private void initilizeThreads() {
-        for(int i =0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(this.sudoku[i][j]==0) this.solverThreads.add(new SolverThread(this.sudoku, i, j));
-            } 
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (this.sudoku[i][j] == 0)
+                    this.solverThreads.add(new SolverThread(this.sudoku, i, j));
+            }
         }
-        this.solverThreads.forEach(i->i.start());
+        this.solverThreads.forEach(i -> i.start());
     }
 
     private void startVisualization() {
-        while(this.solverThreads.stream().anyMatch(i->i.isAlive())) {
+        int itr = 0;
+        while (this.solverThreads.stream().anyMatch(i -> i.isAlive())) {
             // clear the screen
             try {
                 boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
                 ProcessBuilder processBuilder;
-                if(!isWindows)
-                processBuilder = new ProcessBuilder("clear");
+                if (!isWindows)
+                    processBuilder = new ProcessBuilder("clear");
                 else
-                processBuilder = new ProcessBuilder("cls");
+                    processBuilder = new ProcessBuilder("cls");
                 Process process = processBuilder.inheritIO().start();
                 process.waitFor();
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
-
+            System.out.println("Iteration: " + itr++);
             // print sudoku
             System.out.println(this.toString());
             try {
@@ -50,6 +52,14 @@ public class Solver {
 
     @Override
     public String toString() {
-        return Arrays.toString(sudoku); // + ", solverThreads=" + solverThreads + "]";
+        StringJoiner outerJoiner = new StringJoiner("\n");
+        for (int i = 0; i < this.sudoku.length; i++) {
+            StringJoiner innerJoiner = new StringJoiner(" ");
+            for (int j = 0; j < this.sudoku[i].length; j++) {
+                innerJoiner.add(String.valueOf(this.sudoku[i][j]));
+            }
+            outerJoiner.add(innerJoiner.toString());
+        }
+        return outerJoiner.toString() + "\n" + this.solverThreads;
     }
 }
